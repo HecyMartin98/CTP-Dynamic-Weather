b0cb4cdac0a64d0f95b68a616058bf071ff4881b
h <h>
2020-01-13 19:31:59 +0000

    emission rate

---
+++ Dynamic Weather System/Assets/Scripts/PSEditor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PSEditor : MonoBehaviour
+{
+
+    public ParticleSystem rainPS;
+    public GameObject HRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        ParticleSystem rainPS = GetComponent<ParticleSystem>();
+        var emission = rainPS.main;
+
+        rain. = 100.0f;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+
+            
+        }
+    }
+}
