4e9176bb53807e2d07e9e967f8135c7b577581be
h <h>
2020-01-14 20:43:32 +0000

    Demo

--- Dynamic Weather System/Assets/Scripts/PSEditor.cs
+++ Dynamic Weather System/Assets/Scripts/PSEditor.cs
@@ -6,24 +6,74 @@ public class PSEditor : MonoBehaviour
 {
 
     public ParticleSystem rainPS;
-    public GameObject HRB;
+    public ParticleSystem.EmissionModule emissionModule;
 
-    // Start is called before the first frame update
+    public Material skybox1;
+
+    public Light drtLight;
+    public GameObject rainSystem;
+    public GameObject snowSystem;
+    //public GameObject fogSystem;
+
+    
+
+
     void Start()
     {
+        rainSystem.SetActive(false);
+        snowSystem.SetActive(false);
 
-        //ParticleSystem rainPS = GetComponent<ParticleSystem>();
-        //var emission = rainPS.main;
+        RenderSettings.fog = false;
 
-       // rain. = 100.0f;
+        rainPS = GetComponent<ParticleSystem>();
+        emissionModule = rainPS.emission;
+
+        //GetValue();
+        //SetValue();
+
+    }
+
+    void Update()
+    {
+      
     }
 
     void OnTriggerEnter(Collider col)
     {
         if (col.tag == "Player")
         {
-
+            rainSystem.SetActive(true);
+            snowSystem.SetActive(false);
             
+           // fogSystem.SetActive(true);
+            RenderSettings.fog = true;
+            drtLight.intensity = 0.25f;
+            RenderSettings.skybox = skybox1;
+            //emissionModule.rateOverTime = 60;
         }
     }
+
+    void OnTriggerExit(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            rainSystem.SetActive(false);
+            snowSystem.SetActive(true);
+        }
+        
+    }
+
+   
+
+    //void GetValue()
+    //{
+    //    print(string.Format("The constant values are: min {0} max {1}.", emissionModule.rate.constantMin, emissionModule.rate.constantMax));
+    //}
+
+    //void SetValue()
+    //{
+    //    emissionModule.rate = new ParticleSystem.MinMaxCurve(0.0f, 10.0f);
+    //}
+
+
 }
